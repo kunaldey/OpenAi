dba1b31622f4c063e1dc4aec9c0486672d6242ec
kunal <kunaldey1982@gmail.com>
2023-01-18 16:53:13 -0600

    Added client docker file

--- client/script.js
+++ client/script.js
@@ -5,6 +5,7 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
+// const modules = import.meta.globEager('./components/*.js');
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
