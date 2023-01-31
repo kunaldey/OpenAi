8ef85a2a80db377f2597e0acef0b7cdb94989d0d
kunal <kunaldey1982@gmail.com>
2023-01-31 16:59:38 -0600

    Added time function Added copy icon

--- client/script.js
+++ client/script.js
@@ -55,6 +55,7 @@ function chatStripe (isAi, value, uniqueId){
           />
         </div>
         <div class="message" id=${uniqueId}>${value}</div>
+        <button type="button" onclick="${CopyToClipboard(value)}"><img src="assets/copy.png"/></button>
       </div>
     </div>
     `
@@ -128,3 +129,65 @@ function OnInput() {
   this.style.height = 0;
   this.style.height = (this.scrollHeight) + "px";
 }
+
+function Time() {
+
+   // Creating object of the Date class
+   var date = new Date();
+  
+   // Get current hour
+   var hour = date.getHours();
+   // Get current minute
+   var minute = date.getMinutes();
+   // Get current second
+   var second = date.getSeconds();
+  
+   // Variable to store AM / PM
+   var period = "";
+  
+   // Assigning AM / PM according to the current hour
+   if (hour >= 12) {
+   period = "PM";
+   } else {
+   period = "AM";
+   }
+  
+   // Converting the hour in 12-hour format
+   if (hour == 0) {
+   hour = 12;
+   } else {
+   if (hour > 12) {
+   hour = hour - 12;
+   }
+   }
+  
+   // Updating hour, minute, and second
+   // if they are less than 10
+   hour = update(hour);
+   minute = update(minute);
+   second = update(second);
+  
+   // Adding time elements to the div
+   document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
+  
+   // Set Timer to 1 sec (1000 ms)
+   setTimeout(Time, 1000);
+  }
+  
+   // Function to update time elements if they are less than 10
+   // Append 0 before time elements if they are less than 10
+  function update(t) {
+   if (t < 10) {
+   return "0" + t;
+   }
+   else {
+   return t;
+   }
+  }
+  
+  Time();
+
+  function CopyToClipboard(value) {
+    navigator.clipboard.writeText(value);
+    // alert("Copied: " + value);
+  }
